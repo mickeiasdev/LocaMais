8c41f0144660249e623fe43e0c95df3fd285c7c6
Mickeias Dev <mickeiasdev@gmail.com>
2025-10-16 02:02:34 -0300

    commit menssage

--- src/main/java/Model/Veiculo.java
+++ src/main/java/Model/Veiculo.java
@@ -16,8 +16,16 @@ public class Veiculo {
     private boolean disponivel;
 
     // construtor
-    public Veiculo() {
-
+    public Veiculo(int id, String tipo, String marca, String modelo, int ano, String cor, String placa, double valorDiaria) {
+        this.id = id;
+        this.tipo = tipo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+        this.placa = placa;
+        this.valorDiaria = valorDiaria;
+        this.disponivel = true;
     }
 
     // interacoes
@@ -29,12 +37,10 @@ public class Veiculo {
             System.out.println("O carro " + modelo + " não está disponível no momento.");
         }
     }
-
     public void devolver() {
         disponivel = true;
         System.out.println("O carro " + modelo + " foi devolvido e está disponível novamente.");
     }
-
     public void exibirInfo() {
         System.out.println("-------------------------------------------------");
         System.out.println(
